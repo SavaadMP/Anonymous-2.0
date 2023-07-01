3b8644524b3d66e21a101f6e974bf90b1c204f49
SavaadMP <savaadmoorkkanad@gmail.com>
2023-07-01 14:32:16 +0530

    Inicial Commit

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link className="logo" to="/">
+        <h2>🎭 Anonymous</h2>
+      </Link>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <li>
+          <Link to="/">FAQ</Link>
+        </li>
+      </ul>
+      <button>Get Started</button>
+    </header>
+  );
+}
+export default Header;
---
+++ client/src/pages/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+import "./Hero.scss";
+
+function Hero() {
+  return <h1>Hey!! Welcome.</h1>;
+}
+
+export default Hero;
