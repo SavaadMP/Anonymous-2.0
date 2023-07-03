0a9e867addc3d5d83051f32f4c95a0a84ffab914
SavaadMP <savaadmoorkkanad@gmail.com>
2023-07-03 14:28:50 +0530

    frontend: usercode validation completed and impleted some UX

--- client/src/components/UserCodeForm/UserCodeForm.jsx
+++ client/src/components/UserCodeForm/UserCodeForm.jsx
@@ -1,18 +1,78 @@
+import { useEffect, useRef, useState } from "react";
 import UserCodeInput from "../UserCodeInput/UserCodeInput";
 import "./UserCodeForm.scss";
 
 function UserCodeForm() {
+  const codeRef = useRef({});
+  useEffect(() => {
+    codeRef.current[0].focus();
+    codeRef.current[0].addEventListener("paste", pasteUserCode);
+    return codeRef.current[0].removeEventListener("paste", pasteUserCode);
+  }, []);
+
+  const pasteUserCode = (event) => {
+    const pastedText = event.clipboardData.getData("text");
+    const fieldValues = {};
+    Object.keys(code).forEach((keys, index) => {
+      fieldValues[keys] = pastedText[index];
+    });
+    setCode(fieldValues);
+    codeRef.current[5].focus();
+  };
+
+  const [code, setCode] = useState({
+    digitOne: "",
+    digitTwo: "",
+    digitThree: "",
+    digitFour: "",
+    digitFive: "",
+    digitSix: "",
+  });
+
+  function handleChange(event, index) {
+    const { name, value } = event.target;
+    setCode((prev) => ({
+      ...prev,
+      [name]: value.slice(-1),
+    }));
+
+    if (value && index < 5) codeRef.current[index + 1].focus();
+  }
+
+  function handleBackSpace(e, index) {
+    if (e.key === "Backspace") {
+      if (index > 0) codeRef.current[index - 1].focus();
+    }
+  }
+
+  const renderInput = () => {
+    return Object.keys(code).map((digit, index) => {
+      return (
+        <UserCodeInput
+          codeRef={codeRef}
+          key={index}
+          index={index}
+          handleChange={(event) => handleChange(event, index)}
+          name={digit}
+          handleBackSpace={(event) => handleBackSpace(event, index)}
+          value={code[digit]}
+        />
+      );
+    });
+  };
+
+  function verifyOTP(event) {
+    event.preventDefault();
+    const usercode = Object.values(code);
+    var allLetters = usercode.join("");
+
+    console.log(allLetters);
+  }
+
   return (
-    <form>
-      <div className="input_field">
-        <UserCodeInput />
-        <UserCodeInput />
-        <UserCodeInput />
-        <UserCodeInput />
-        <UserCodeInput />
-        <UserCodeInput />
-      </div>
-      <button>Verify & Proceed</button>
+    <form onSubmit={verifyOTP}>
+      <div className="input_field">{renderInput()}</div>
+      <button type="submit">Verify & Proceed</button>
     </form>
   );
 }
--- client/src/components/UserCodeInput/UserCodeInput.jsx
+++ client/src/components/UserCodeInput/UserCodeInput.jsx
@@ -1,5 +1,22 @@
-const UserCodeInput = () => {
-  return <input type="text" />;
+const UserCodeInput = ({
+  name,
+  handleChange,
+  codeRef,
+  index,
+  handleBackSpace,
+  value,
+}) => {
+  return (
+    <input
+      type="text"
+      ref={(element) => (codeRef.current[index] = element)}
+      name={name}
+      onChange={handleChange}
+      onKeyUp={handleBackSpace}
+      value={value}
+      required={true}
+    />
+  );
 };
 
 export default UserCodeInput;
